78a7dffca1f29f0570f643136d997cf13952c0a8
valentimaldana <148530069+valentimaldana@users.noreply.github.com>
2023-10-23 08:28:35 -0300

    Update

---
+++ js/script2.js
@@ -0,0 +1,17 @@
+function carregar2() {
+    window.alert('NoScript Blocked ? Habilite o Som no Rodapé')
+   
+    var tort = window.document.getElementById('tort')
+  
+  
+    var data = new Date()
+ 
+   
+    tort.innerHTML = ` ${data}  `
+    
+   
+     
+  }
+
+
+
